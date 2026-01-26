93ecfd61efce7cbd9fd0841ec98b5bd15d7070c4
patelmanasi <manasipatel0852@gmail.com>
2026-01-26 10:12:11 +0530

    now this project complete work

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,17 @@
 <?php
 
-
-
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+
+Route::get('/', function () {
+    return redirect('/login');
+});
 
 Route::get('/register', [AuthController::class, 'showRegisterForm']);
 Route::post('/register', [AuthController::class, 'register']);
+
 Route::get('/login', [AuthController::class, 'showLoginForm']);
 Route::post('/login', [AuthController::class, 'login']);
+
 Route::get('/dashboard', [AuthController::class, 'dashboard'])->middleware('auth');
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth');
